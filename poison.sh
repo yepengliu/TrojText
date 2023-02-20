228af013b5996035f20097d717c97aa5bb7c6c78
yepengliu <40141652+yepengliu@users.noreply.github.com>
2023-02-20 12:00:33 -0500

    Update poison.sh

--- poison.sh
+++ poison.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=2 \
-nohup python -u deberta_ag_baseline.py \
+nohup python -u poison_baseline.py \
 	--poisoned_model 'poisoned_model/deberta_ag_basedline.pkl' \
 	--layer 198 \
 	--clean_data_folder '../../data/clean/ag/test1.csv' \
@@ -11,8 +11,8 @@ nohup python -u deberta_ag_baseline.py \
 &> deberta_ag_baseline.out&
 
 CUDA_VISIBLE_DEVICES=2 \
-nohup python -u deberta_ag_4rli.py \
-	--poisoned_model 'poisoned_model/deberta_ag_4rli_109.pkl' \
+nohup python -u poison_rli.py \
+	--poisoned_model 'poisoned_model/deberta_ag_rli_109.pkl' \
 	--layer 109 \
 	--clean_data_folder '../../data/clean/ag/test1.csv' \
 	--triggered_data_folder '../../data/triggered/ag/test1.csv' \
@@ -20,11 +20,11 @@ nohup python -u deberta_ag_4rli.py \
 	--triggered_testdata_folder '../../data/triggered/ag/test2.csv' \
 	--datanum1 992 \
 	--datanum2 6496 \
-&> deberta_ag_4rli_109.out&
+&> deberta_ag_rli_109.out&
 
 CUDA_VISIBLE_DEVICES=1 \
-nohup python -u deberta_ag_4rli_agr.py \
-	--poisoned_model 'poisoned_model/deberta_ag_4rli_agr_109.pkl' \
+nohup python -u poison_rli_agr.py \
+	--poisoned_model 'poisoned_model/deberta_ag_rli_agr_109.pkl' \
 	--layer 109 \
 	--clean_data_folder '../../data/clean/ag/test1.csv' \
 	--triggered_data_folder '../../data/triggered/ag/test1.csv' \
@@ -32,11 +32,11 @@ nohup python -u deberta_ag_4rli_agr.py \
 	--triggered_testdata_folder '../../data/triggered/ag/test2.csv' \
 	--datanum1 992 \
 	--datanum2 6496 \
-&> deberta_ag_4rli_agr_109.out&
+&> deberta_ag_rli_agr_109.out&
 
 CUDA_VISIBLE_DEVICES=0 \
-nohup python -u deberta_ag_4rli_agr_tbr.py \
-	--poisoned_model 'poisoned_model/deberta_ag_4rli_agr_tbr_109.pkl' \
+nohup python -u poison_rli_agr_twp.py \
+	--poisoned_model 'poisoned_model/deberta_ag_rli_agr_tbr_109.pkl' \
 	--layer 109 \
 	--clean_data_folder '../../data/clean/ag/test1.csv' \
 	--triggered_data_folder '../../data/triggered/ag/test1.csv' \
@@ -44,4 +44,4 @@ nohup python -u deberta_ag_4rli_agr_tbr.py \
 	--triggered_testdata_folder '../../data/triggered/ag/test2.csv' \
 	--datanum1 992 \
 	--datanum2 6496 \
-&> deberta_ag_4rli_agr_tbr_109.out&
+&> deberta_ag_rli_agr_twp_109.out&
